d885c2d3f9ef912405302c920266d8439b64cc19
Daniel Carvalho <daniel.devop@gmail.com>
2022-08-27 21:54:56 -0300

    Classe cpf

---
+++ src/main/java/dev/unscrud/escola/CPF.java
@@ -0,0 +1,21 @@
+package dev.unscrud.escola;
+
+public class CPF {
+  private String numero;
+
+  public String getEndereco() {
+    return numero;
+  }
+
+  public CPF(String numero) {
+    if (numero == null || isInvalido(numero)) {
+      throw new IllegalArgumentException("CPF Inválido!");
+    }
+    this.numero = numero;
+  }
+
+  private boolean isInvalido(String numero) {
+    final String REGEX = "\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{}";
+    return !numero.matches(REGEX);
+  }
+}
