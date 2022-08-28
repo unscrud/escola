549ebb646dd9a77fb92a73eb5d62cd515ea859bb
Daniel Carvalho <daniel.devop@gmail.com>
2022-08-28 13:00:09 -0300

    adicionando telefone à classe aluno

--- src/main/java/dev/unscrud/escola/Aluno.java
+++ src/main/java/dev/unscrud/escola/Aluno.java
@@ -1,7 +1,15 @@
 package dev.unscrud.escola;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aluno {
   private CPF cpf;
   private String nome;
   private Email email;
+  private List<Telefone> telefones = new ArrayList<>();
+
+  public void adicionaTelefone(String ddd, String numero) {
+    this.telefones.add(new Telefone(ddd, numero));
+  }
 }
