b159f2c25263adcbd3b408bd9ef6b6fff98be4be
Daniel Carvalho <daniel.devop@gmail.com>
2022-08-28 12:59:23 -0300

    classe de telefone

---
+++ src/main/java/dev/unscrud/escola/Telefone.java
@@ -0,0 +1,19 @@
+package dev.unscrud.escola;
+
+public class Telefone {
+  private String ddd;
+  private String numero;
+
+  public Telefone(String ddd, String numero) {
+    this.ddd = ddd;
+    this.numero = numero;
+  }
+
+  public String getDdd() {
+    return ddd;
+  }
+
+  public String getNumero() {
+    return numero;
+  }
+}
