998aecedac51a1f6aacc78775d0c9f87bfe418ea
Daniel Carvalho <daniel.devop@gmail.com>
2022-08-27 21:45:14 -0300

    classe de email

---
+++ src/main/java/dev/unscrud/escola/Email.java
@@ -0,0 +1,21 @@
+package dev.unscrud.escola;
+
+public class Email {
+  private String endereco;
+
+  public String getEndereco() {
+    return endereco;
+  }
+
+  public Email(String endereco) {
+    if (endereco == null || isInvalido(endereco)) {
+      throw new IllegalArgumentException("Email Inválido!");
+    }
+    this.endereco = endereco;
+  }
+
+  private boolean isInvalido(String endereco) {
+    final String REGEX = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+    return !endereco.matches(REGEX);
+  }
+}
