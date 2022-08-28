2c8464376c903cc4f8a37738e4b0c0b96111443f
Daniel Carvalho <daniel.devop@gmail.com>
2022-08-28 13:00:52 -0300

    validacao de telefone

--- src/main/java/dev/unscrud/escola/Telefone.java
+++ src/main/java/dev/unscrud/escola/Telefone.java
@@ -5,6 +5,15 @@ public class Telefone {
   private String numero;
 
   public Telefone(String ddd, String numero) {
+    if (ddd == null || numero == null)
+      throw new IllegalArgumentException("DDD e número são obrigatórios");
+
+    if (!ddd.matches("\\d{2}"))
+      throw new IllegalArgumentException("DDD inválido");
+
+    if (!numero.matches("\\d{8}|\\d{9}"))
+      throw new IllegalArgumentException("Número inválido");
+
     this.ddd = ddd;
     this.numero = numero;
   }
