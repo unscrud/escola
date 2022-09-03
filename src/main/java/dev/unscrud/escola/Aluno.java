c4e5381e0c02de425b0cbb8beaaaf477ef71ee0c
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-03 08:00:27 -0300

    construtor e getters

--- src/main/java/dev/unscrud/escola/Aluno.java
+++ src/main/java/dev/unscrud/escola/Aluno.java
@@ -9,7 +9,30 @@ public class Aluno {
   private Email email;
   private List<Telefone> telefones = new ArrayList<>();
 
+  public Aluno(CPF cpf, String nome, Email email) {
+    this.cpf = cpf;
+    this.nome = nome;
+    this.email = email;
+  }
+
   public void adicionaTelefone(String ddd, String numero) {
     this.telefones.add(new Telefone(ddd, numero));
   }
+
+  public CPF getCpf() {
+    return cpf;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public Email getEmail() {
+    return email;
+  }
+
+  public List<Telefone> getTelefones() {
+    return telefones;
+  }
+
 }
