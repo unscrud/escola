f4ba257197b67a3b1d6c25255dd085e4ff2e0a13
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-03 07:35:38 -0300

    Teste de telefones

---
+++ src/test/java/dev/unscrud/escola/TelefoneTest.java
@@ -0,0 +1,80 @@
+package dev.unscrud.escola;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class TelefoneTest {
+  private final String[] dddsInvalidos = { null, "1", "a4", "333" };
+  private final String[] telefonesInvalidos = { null, "", "", "" };
+  private final String[] dddsValidos = { "21", "22", "23" };
+  private final String[] telefonesValidos = { "12345678", "123456789" };
+
+  @Test
+  void naoDeveriaAceitarDDDETelefonesInvalidos() {
+    List<Telefone> telefones = new ArrayList<>();
+    for (String ddd : dddsInvalidos) {
+      for (String numero : telefonesInvalidos) {
+        try {
+          telefones.add(new Telefone(ddd, numero));
+        } catch (IllegalArgumentException e) {
+          assertThrows(IllegalArgumentException.class,
+              () -> new Telefone(ddd, numero));
+        }
+      }
+    }
+    assertEquals(0, telefones.size());
+  }
+
+  @Test
+  void naoDeveriaAceitarDDDInvalidoComTelefoneValido() {
+    List<Telefone> telefones = new ArrayList<>();
+    for (String ddd : dddsInvalidos) {
+      for (String numero : telefonesValidos) {
+        try {
+          telefones.add(new Telefone(ddd, numero));
+        } catch (IllegalArgumentException e) {
+          assertThrows(IllegalArgumentException.class,
+              () -> new Telefone(ddd, numero));
+        }
+      }
+    }
+    assertEquals(0, telefones.size());
+  }
+
+  @Test
+  void naoDeveriaAceitarDDDValidoComTelefoneInvalido() {
+    List<Telefone> telefones = new ArrayList<>();
+    for (String ddd : dddsValidos) {
+      for (String numero : telefonesInvalidos) {
+        try {
+          telefones.add(new Telefone(ddd, numero));
+        } catch (IllegalArgumentException e) {
+          assertThrows(IllegalArgumentException.class,
+              () -> new Telefone(ddd, numero));
+        }
+      }
+    }
+    assertEquals(0, telefones.size());
+  }
+
+  @Test
+  void deveriaAceitarDDDeTelefonesValidos() {
+    List<Telefone> telefones = new ArrayList<>();
+    for (String ddd : dddsValidos) {
+      for (String numero : telefonesValidos) {
+        try {
+          telefones.add(new Telefone(ddd, numero));
+        } catch (IllegalArgumentException e) {
+          assertThrows(IllegalArgumentException.class,
+              () -> new Telefone(ddd, numero));
+        }
+      }
+    }
+    assertEquals(6, telefones.size());
+  }
+}
