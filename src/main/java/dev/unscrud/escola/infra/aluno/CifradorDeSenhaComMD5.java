a4852b46abc72e40e10bccf5d417ed681bcf4d73
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-05 13:03:36 -0300

    implementacao dos métodos do cifrador

--- src/main/java/dev/unscrud/escola/infra/aluno/CifradorDeSenhaComMD5.java
+++ src/main/java/dev/unscrud/escola/infra/aluno/CifradorDeSenhaComMD5.java
@@ -1,19 +1,31 @@
 package dev.unscrud.escola.infra.aluno;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import dev.unscrud.escola.dominio.aluno.CifradorDeSenha;
 
 public class CifradorDeSenhaComMD5 implements CifradorDeSenha {
 
   @Override
   public String cifrarSenha(String senha) {
-    // TODO Auto-generated method stub
-    return null;
+    try {
+      MessageDigest md = MessageDigest.getInstance("MD5");
+      md.update(senha.getBytes());
+      byte[] bytes = md.digest();
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < bytes.length; i++) {
+        sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+      }
+      return sb.toString();
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException("erro ao gerar hash de senha");
+    }
   }
 
   @Override
   public boolean validarSenhaCifrada(String senhaCifrada, String senhaAberta) {
-    // TODO Auto-generated method stub
-    return false;
+    return senhaCifrada.equals(cifrarSenha(senhaAberta));
   }
 
 }
