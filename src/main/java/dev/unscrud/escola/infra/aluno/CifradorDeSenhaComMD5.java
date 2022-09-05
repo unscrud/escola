5b491dcf706cb44f22c7af33300fb8a03a632298
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-05 11:04:48 -0300

    implementacao do cifrador

---
+++ src/main/java/dev/unscrud/escola/infra/aluno/CifradorDeSenhaComMD5.java
@@ -0,0 +1,19 @@
+package dev.unscrud.escola.infra.aluno;
+
+import dev.unscrud.escola.dominio.aluno.CifradorDeSenha;
+
+public class CifradorDeSenhaComMD5 implements CifradorDeSenha {
+
+  @Override
+  public String cifrarSenha(String senha) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public boolean validarSenhaCifrada(String senhaCifrada, String senhaAberta) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+}
